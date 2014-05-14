5e95021a0e06071a1d5abcb3e36ad37a15a8db7a
Nils <area51.nils@googlemail.com>
2014-05-14 23:21:25 +0200

    Added saving of files and first calculation of sigma. Needs improvement

--- ex2/2.py
+++ ex2/2.py
@@ -7,6 +7,7 @@ Created on Fri May 09 10:07:35 2014
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os.path
 
 # Globale Variablendefintionen
 sqrtS = 2000
@@ -18,11 +19,15 @@ GF = 10**(-5)
 class Event:
     """ Klasse zur Speicherung und Erzeugung eines Events """
 
+    outputFile = "eventsData.npy"
+
     def __init__(self):
         self.p0e, self.p1e, self.p2e, self.p3e = 0, 0, 0, 0
         self.p0n, self.p1n, self.p2n, self.p3n = 0, 0, 0, 0
         self.x1, self.x2, self.tau, self.y, self.sqrt_s_hut = 0, 0, 0, 0, 0
         self.cos_theta, self.phi = 0, 0
+
+        self.dsigma = np.nan
 
         self.get_event()
 
@@ -55,7 +60,7 @@ class Event:
         # Berechnen der gleichverteilten Zufallsvariablen
         random = self.random(4)
         # tau zwischen 50/SqrtS und 100/SqrtS
-        self.tau = 50**2/sqrtS**2 * random[0] + 50**2/sqrtS**2
+        self.tau = (100**2 - 50**2)/sqrtS**2 * random[0] + 50**2/sqrtS**2
         # abs(y) kleiner log(tau)/2
         self.y = np.log(self.tau)*random[1] - np.log(self.tau)/2.0
         # cos Theta zwischen -1 und 1
@@ -68,7 +73,6 @@ class Event:
         self.x1 = self.tau / self.x2
         self.sqrt_s_hut = np.sqrt(self.tau) * sqrtS
 
-        # TODO: Berechnung von pe und pn!
         # Aus y = 1/2 log(1 + beta / 1 - beta)
         beta = (np.exp(2 * self.y) - 1)/(np.exp(2 * self.y) + 1)
         gamma = 1/np.sqrt(1 - beta**2)
@@ -98,39 +102,58 @@ class Event:
     def get_dsigma(self):
         """ Berechnet dSigma für das gegebene Event
         """
-        return self._f(self.x1) * self._f(self.x2) * 1/(32*np.pi**2) * \
-               self.get_sum_m() * 1/(2*self.sqrt_s_hut**2) * 1/(2*np.pi)**2 * 1/(4*self.p0e*self.p0n)
+        if self.dsigma is np.nan:
+            self.dsigma = self._f(self.x1) * self._f(self.x2) * 1/(32*np.pi**2) * self.get_sum_m() * 1/(2*self.sqrt_s_hut**2) * 1/(2*np.pi)**2 * 1/(4*self.p0e*self.p0n)
+            return self.dsigma
+        else:
+            return self.dsigma
+
+    def __getitem__(self, item):
+        return (self.p0e, self.p1e, self.p2e, self.p3e,  # 0, 1, 2, 3
+                self.p0n, self.p1n, self.p2n, self.p3n,  # 4, 5, 6, 7
+                self.x1, self.x2,                        # 8, 9
+                self.tau, self.y,                        # 10, 11
+                self.sqrt_s_hut,                         # 12
+                self.cos_theta, self.phi,                # 13, 14
+                self.get_dsigma())[item]                 # 15
 
 
-def get_max_dsigma():
-    max_dsigma = -1
-    for i in range(10000):
-        event = Event()
-        max_dsigma = np.maximum(event.get_dsigma(), max_dsigma)
-    return max_dsigma
-    
-# TODO: implementieren
-#def plot(events):
-    #plt.hist(np.sqrt(events[:,1]**2 + events[:,2]**2 + events[:,3]**2), bins=40)
-    #plt.hist(np.sqrt(events[:,5]**2 + events[:,6]**2), bins=40)
+def plot(plot_events):
+    good_events = np.array([list(x) for x in plot_events])
+    plt.hist(good_events[:, 12], bins=40)
+    plt.show()
+    plt.hist(np.sqrt(good_events[:, 1]**2 + good_events[:, 2]**2), bins=40)
+    plt.show()
         
 
 if __name__ == '__main__':
-    #maxDSigma = get_max_dsigma()
-    maxDSigma = 4.16315676394e-06
-    sigma = 0
-    counter = 0
+
+    debug = 1
+
     events = list()
-    for i in range(10000):
-        x = Event()
-        #dSigma = x.get_dsigma()
-        #if Event.random() < dSigma/maxDSigma:
-        #    sigma += dSigma
-        #    counter += 1.0
-        #    events.append(x)
-    #print(sigma/counter, sigma/counter*0.3894/1000.0, counter)
-    
-    #events = np.load('eventsData.npy')
-    #plot(events)
-    
-    #np.save("eventsData", np.array(events))
+    goodEvents = list()
+
+    if debug == 0:
+        print("calculating new points...")
+        maxDSigma = 0
+        for i in range(100000):
+            x = Event()
+            dSigma = x.get_dsigma()
+            events.append(x)
+            maxDSigma = np.maximum(maxDSigma, dSigma)
+
+        for x in events:
+            if Event.random() <= x.get_dsigma()/maxDSigma:
+                goodEvents.append(x)
+
+    if os.path.isfile(Event.outputFile):
+        print("reading old points...")
+        loadedEvents = np.load(Event.outputFile)
+        goodEvents.extend(loadedEvents)
+
+    np.save(Event.outputFile, goodEvents)
+
+    sigma = sum([x.get_dsigma()/len(goodEvents) for x in goodEvents])
+    print("sigma =", sigma*0.3894/1000.0, ", count =", len(goodEvents))
+
+    plot(goodEvents)
