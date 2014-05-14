52b6e48f0d6ab7f2961a042d21bdf005fbb8ef34
Nils <area51.nils@googlemail.com>
2014-05-14 17:39:00 +0200

    Added some comments, a class for the Events and started implementing the other parameter scheme

--- ex2/2.py
+++ ex2/2.py
@@ -2,106 +2,123 @@
 """
 Created on Fri May 09 10:07:35 2014
 
-@author: s0cke
+@author: s0cke, nilpferd1991
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Globale Variablendefintionen
 sqrtS = 2000
 MassW = 80
 GammaW = 2
 GF = 10**(-5)
 
-#Partonverteilung
-def f(x, s):
-    return x**(0.2-0.3*np.log(s))*(1-x)**0.1
-    
-def plotF(log=True):
-    t = np.arange(0., 1., 0.001)
-    plt.plot(t, f(t, 2000))
-    if log == True:
-        plt.yscale('log')
+
+class Event:
+    """ Klasse zur Speicherung und Erzeugung eines Events """
+
+    def __init__(self):
+        self.p0e, self.p1e, self.p2e, self.p3e = 0, 0, 0, 0
+        self.p0n, self.p1n, self.p2n, self.p3n = 0, 0, 0, 0
+        self.x1, self.x2, self.tau, self.y, self.sqrt_s_hut = 0, 0, 0, 0, 0
+        self.cos_theta, self.phi = 0, 0
+
+        self.get_event()
+
+    @staticmethod
+    def random(size=-1):
+        """ Gibt eine Anzahl an zufälligen Elementen zurück
+        :rtype : np.array
+        :param size: Die Anzahl der zufälligen Elemente
+        TODO: Eigenen Zufallszahlengenerator implementieren
+        """
+
+        if size == -1:
+            return np.random.random()
+        else:
+            return np.random.random(size, )
+
+    def _f(self, x):
+        """ Berechnet die Parton-pdf
+
+        :param x: der Partonenbruchteil
+        :rtype : double
+        """
+        return x**(0.2-0.3*np.log(self.sqrt_s_hut**2))*(1-x)**0.1
+
+    def get_event(self):
+        """ Gibt ein zufällig gewürfeltes Event zurück
+        Ändert:
+        p0e, p1e, p2e, p3e, p0n, p1n, p2n, p3n, x1, x2, Tau, y, sqrtSHut
+
+        TODO: Andere Variablen verwenden!
+        :rtype : none
+        """
+
+        # Berechnen der gleichverteilten Zufallsvariablen
+        random = self.random(4)
+        # tau zwischen 50/SqrtS und 100/SqrtS
+        self.tau = 50/sqrtS * random[0] + 50/sqrtS
+        # y zwischen -5 und 5
+        self.y = 10*random[1] - 5
+        # cos Theta zwischen -1 und 1
+        self.cos_theta = 2*random[2] - 1
+        # phi zwischen 0 und 2 pi
+        self.phi = 2 * np.pi * random[3]
+
+        # Berechnen der restlichen Parameter
+
         
 
-def getEvent():
-    #Tau,y,p2e,p3e = np.random.random(4,)
-    #y = y*10 - 5
-    #x2 = np.sqrt(Tau/np.exp(2*np.abs(y)))
-    #x1 = Tau/x2
+    def get_sum_m(self):
+        return 2*GF**2*MassW**8*(1+self.cos_theta)**2 / ((self.sqrt_s_hut**2-MassW**2)**2 + MassW**2*GammaW**2)
 
-    x1,x2,p2e,p3e = np.random.random(4,)
-    y = 0.5*np.log(x1/x2)
-    Tau = x1*x2
-    sqrtSHut = np.sqrt(Tau)*sqrtS
-    p2e = p2e*sqrtSHut - sqrtSHut/2.0
-    p3e = p3e*sqrtSHut - sqrtSHut/2.0
-    p2n = -p2e
-    p3n = sqrtS/2. * (x1-x2)-p3e
-    
-    if (sqrtS**2*(-p2e**2*(x1+x2)**2 + x1*(-2.0*p3e+sqrtS*x1)*x2*(2*p3e+sqrtS*x2)) < 0):
-        return getEvent()
-    else:
-        p1e = np.random.choice([1,-1]) * np.sqrt(sqrtS**2*(-p2e**2*(x1+x2)**2 + x1*(-2.0*p3e+sqrtS*x1)*x2*(2*p3e+sqrtS*x2))) / np.sqrt(sqrtS**2*(x1+x2)**2)
-        p1n = -p1e
+    def get_dsigma(self):
+        """ Berechnet dSigma für das gegebene Event
+        TODO: Jakobideterminante implementieren!
+        """
+        return self._f(self.x1) * self._f(self.x2) * \
+               self.get_sum_m() * 1/(2*self.sqrt_s_hut**2) * 1/(2*np.pi)**2 * 1/(4*self.p0e*self.p0n)
         
-        p0e = np.sqrt(p1e**2+p2e**2+p3e**2)
-        p0n = np.sqrt(p1n**2+p2n**2+p3n**2)
-        #sqrtSHut = np.sqrt(2*(p0e*p0n-p1e*p1n-p2e*p2n-p3e*p3n))
-        
-        return p0e, p1e, p2e, p3e, p0n, p1n, p2n, p3n, x1, x2, Tau, y, sqrtSHut
-        
-        
-def cuts(p0e, p1e, p2e, p3e, p0n, p1n, p2n, p3n, x1, x2, Tau, y, sqrtSHut):
-    if ((sqrtSHut < 50) or (sqrtSHut > 100)):
-        return False
-    if np.abs(y) >= 5:
-        return False
-    return True
-    
+    # TODO: Sollte in get_event implementiert werdn!
+    #def cuts(p0e, p1e, p2e, p3e, p0n, p1n, p2n, p3n, x1, x2, Tau, y, sqrtSHut):
+    #    if ((sqrtSHut < 50) or (sqrtSHut > 100)):
+    #        return False
+    #    if np.abs(y) >= 5:
+    #        return False
+    #    return True
 
-def getDSigma():
-    x = getEvent()
-    while (not cuts(*x)):
-        x = getEvent()
-    #print x
-    cosTheta = x[3] / np.sqrt(x[1]**2 + x[2]**2 + x[3]**2)
-    sum = 2*GF**2*MassW**8*(1+cosTheta)**2 / ((x[12]-MassW**2)**2 + MassW**2*GammaW**2)
-    #print sum
-    
-    dSigma = f(x[8],x[12]) * f(x[9],x[12]) * sum * 1/(2*x[12]) * 1/(2*np.pi)**2 * 1/(4*x[0]*x[4])
-    #print dSigma
-    return dSigma, x
-    
 
-def getMaxDSigma():
-    maxDSigma = -1
+def get_max_dsigma():
+    max_dsigma = -1
     for i in range(10000):
-        maxDSigma = np.maximum(getDSigma()[0], maxDSigma)
-    print maxDSigma
+        event = Event()
+        max_dsigma = np.maximum(event.get_dsigma(), max_dsigma)
+    return max_dsigma
     
-    
-def plot(events):
+# TODO: implementieren
+#def plot(events):
     #plt.hist(np.sqrt(events[:,1]**2 + events[:,2]**2 + events[:,3]**2), bins=40)
-    plt.hist(np.sqrt(events[:,5]**2 + events[:,6]**2), bins=40)
+    #plt.hist(np.sqrt(events[:,5]**2 + events[:,6]**2), bins=40)
         
 
 if __name__ == '__main__':
+    #maxDSigma = get_max_dsigma()
     maxDSigma = 4.16315676394e-06
     sigma = 0
     counter = 0
-    #events = list()
-    #for i in range(10000):
-    #    dSigma,x = getDSigma()
-    #    if np.random.random() < dSigma/maxDSigma:
-    #        sigma += dSigma
-    #        counter += 1.0
-    #        events.append(np.array(x))
-    #print sigma/counter
-    #print sigma/counter*0.3894/1000.0
-    #print counter
+    events = list()
+    for i in range(10000):
+        x = Event()
+        dSigma = x.get_dsigma()
+        if Event.random() < dSigma/maxDSigma:
+            sigma += dSigma
+            counter += 1.0
+            events.append(x)
+    print(sigma/counter, sigma/counter*0.3894/1000.0, counter)
     
-    events = np.load('eventsData.npy')
-    plot(events)
+    #events = np.load('eventsData.npy')
+    #plot(events)
     
     #np.save("eventsData", np.array(events))
